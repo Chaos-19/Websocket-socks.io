9f83a210a773eb0d3cb0ff199ae71b3da5c7a6a3
Chaos-19 <kalgetachew375@gmail.com>
2023-12-18 12:39:52 +0300

    lesson-3

--- server/index.js
+++ server/index.js
@@ -18,7 +18,31 @@ const io = new Server(expressServer, {
 io.on("connection", socket => {
     console.log(`user ${socket.id} connected`);
 
+    //upon connection only to user
+    socket.emit("message", "welcome to chat app");
+
+    //upOn notify other user the new User connected
+    socket.broadcast.emit(
+        // broadcast will send to all user other user
+        "message",
+        `user ${socket.id.substring(0, 5)} connected`
+    );
+
+    //listening for message event
     socket.on("message", msg => {
         io.emit("message", `${socket.id.substring(0, 5)} : ${msg}`);
+    });
+
+    //disconnect when user disconnect
+    io.on("disconnect", () => {
+        socket.broadcast.emit(
+            "disconnect",
+            `User ${socket.id.substring(0, 5)} disconnect`
+        );
+    });
+
+    //listening for activity event
+    socket.on("activity", msg => {
+        socket.broadcast.emit("activity", `${msg}`);
     });
 });
